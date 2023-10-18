b8b1515bdd1bab0bd1f21afabc5adf8f75da9f90
jonas <jonassaraivasantos@gmail.com>
2023-10-18 20:38:45 -0300

    criado projeto inicial Controle de Fluxo

--- tipos-e-variaveis/src/TiposVariaveis.java
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -1,21 +1,7 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
 
-        SmartTV smartTV = new SmartTV();
-
-        System.out.println("TV Ligada? " + smartTV.ligada);
-        smartTV.mudarCanal(7);
-        System.out.println("Canal Atual? " + smartTV.canal);
-        System.out.println("Volume Atual? " + smartTV.volume);
-
-        smartTV.aumentarVolume();
-        System.out.println("Novo Volume " + smartTV.volume);
-        smartTV.diminuirVolume();
-        System.out.println("Novo Volume " + smartTV.volume);
-
-        smartTV.aumentarCanal();
-        smartTV.aumentarCanal();
-        System.out.println("Canal Atual? " + smartTV.canal);
+        
     }
 
 }
